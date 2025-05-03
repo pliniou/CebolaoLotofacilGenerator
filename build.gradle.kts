ff0647c306dfaceab86a0c340251ae71ba2a82f1
bsbplinio <bsbplinio@gmail.com>
2025-05-03 18:41:38 +0000

    Atualização 0.0.6 via Firebase Studio

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,3 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-}
-kotlin {
-    jvmToolchain(17)}
