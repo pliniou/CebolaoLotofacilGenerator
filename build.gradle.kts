eec5b3320245443faf17f4e64a75d62cef0e1588
bsbplinio <bsbplinio@gmail.com>
2025-05-03 18:43:34 +0000

    Versão 0.0.6.1 via Firebase Studio

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+}
