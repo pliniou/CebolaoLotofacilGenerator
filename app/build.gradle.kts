0be1abcbafae6d44b75587d844a0f9eea9aa618c
bsbplinio <bsbplinio@gmail.com>
2025-05-03 20:05:19 +0000

    Versão 0.0.7 via Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,4 +60,5 @@ dependencies {
     implementation("androidx.datastore:datastore-preferences:1.1.5")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("androidx.navigation:navigation-compose:2.7.7")
-}
+}implementation(androidx.compose.material3:material3:1.2.0)
+sync.gradle
