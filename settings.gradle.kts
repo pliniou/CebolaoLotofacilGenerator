5dfae0c24b9ffa0d24baffa1469131514002ecfb
bsbplinio <bsbplinio@gmail.com>
2025-05-04 17:47:40 +0000

    Atualizacao 0.1.5 via Firebase

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,14 @@
 pluginManagement {
     repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        maven {
+            setUrl("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+            content {
+                includeGroup("org.jetbrains.compose")
+                includeGroup("org.jetbrains.kotlinx")
+            }
+        }
         google {
             content {
                 includeGroupByRegex("com\\.android.*")
@@ -7,9 +16,7 @@ pluginManagement {
                 includeGroupByRegex("androidx.*")
             }
         }
-        mavenCentral()
-        gradlePluginPortal()
-    }
+   }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
