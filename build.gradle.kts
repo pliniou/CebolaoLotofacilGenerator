206aec94cdd13b8308d9b706f38c1dd4ffebf4fb
bsbplinio <bsbplinio@gmail.com>
2025-05-05 17:36:47 +0000

    0.4.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,3 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
-
-configurations.all {
-    resolutionStrategy.eachDependency {
-        if (requested.group == "org.jetbrains.kotlin") {
-            useVersion("1.9.0")
-        }
-    }
-}
