5d29a8c7e0d111ff299553c450f638431c4d2a53
bsbplinio <bsbplinio@gmail.com>
2025-05-08 11:48:35 +0000

    0.8.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.ksp)    
 }
