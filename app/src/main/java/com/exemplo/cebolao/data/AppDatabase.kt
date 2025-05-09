3dd2db4af8bb4638ca6227ff07a2b30e87d451dc
bsbplinio <bsbplinio@gmail.com>
2025-05-09 14:26:46 +0000

    0.9.9

--- app/src/main/java/com/exemplo/cebolao/data/AppDatabase.kt
+++ app/src/main/java/com/exemplo/cebolao/data/AppDatabase.kt
@@ -3,10 +3,10 @@ package com.exemplo.cebolao.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.exemplo.cebolao.data.TypeConverter
+import com.exemplo.cebolao.utils.ListConverter
 
 @Database(entities = [JogoEntity::class], version = 1, exportSchema = false)
-@TypeConverters(TypeConverter::class)
+@TypeConverters(ListConverter::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun jogoDao(): JogoDao
 }
