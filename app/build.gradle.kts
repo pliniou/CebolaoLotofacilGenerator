9fb368975c5190af99950d8dd76ac194aebd264e
bsbplinio <bsbplinio@gmail.com>
2025-05-09 14:39:02 +0000

    0.9.9.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,35 +48,38 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.lifecycle.livedata.ktx)
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
     testImplementation(libs.junit)
     testImplementation("androidx.test.ext:junit:1.1.5")
-
-
-    implementation(libs.androidx.room.runtime)
-    implementation(libs.androidx.room.ktx)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.kotlinx.coroutines.core)    
+    implementation(libs.kotlinx.coroutines.core)
     implementation("androidx.compose.ui:ui:1.5.4")
     implementation("androidx.compose.ui:ui-graphics:1.6.0")
     debugImplementation("androidx.compose.ui:ui-tooling:1.5.4")
     implementation("androidx.compose.material:material-icons-extended:1.5.4")
 
-    
     implementation(libs.androidx.activity.ktx)
-
-    implementation(libs.androidx.navigation.compose)
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-    implementation("androidx.navigation:navigation-compose:2.7.5")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    debugImplementation(libs.androidx.ui.tooling.preview)
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
 
-    ksp(libs.androidx.room.compiler)
-    debugImplementation(libs.androidx.ui.tooling.preview)    
+    // Room
+    implementation("androidx.room:room-runtime:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    // Lifecycle + ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    // Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.6")
+    // Gson
+    implementation("com.google.code.gson:gson:2.10.1")
+    // DataStore
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
