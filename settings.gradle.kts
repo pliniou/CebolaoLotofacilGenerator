48704a12b43dd1136f37da9e0cf0bf23fa1a5c88
bsbplinio <bsbplinio@gmail.com>
2025-05-10 12:57:57 +0000

    1.0.5

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,11 +25,6 @@ pluginManagement {
    }
 }
 dependencyResolutionManagement {
-    versionCatalogs {
-        create("libs") {
-            from(files("gradle/libs.versions.toml"))
-        }
-    }
     repositories {
         google()
         mavenCentral()
