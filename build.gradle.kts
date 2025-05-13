913c68ed281dcc38676811296da2d0a3f143b92a
pliniou <bsbplinio@gmail.com>
2025-05-12 21:06:51 -0300

    0.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
-val gradleVersion = "8.3.0" // Atualizado de 8.0.0 para melhor compatibilidade com compileSdk 34
-val kotlinVersion = "1.9.23" // Corrigido para coincidir com libs.versions.toml
-
 buildscript {
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:$gradleVersion")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("com.android.tools.build:gradle:8.3.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
